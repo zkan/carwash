b3f44bf1c09dc2ea7b7989acdf4e45942ac0179f
prontokan <kan@prontomarketing.com>
2014-12-09 08:52:37 +0700

    add more test cases (not allow to set None to first name and last name) :bear:

--- carwash/members/tests/test_models.py
+++ carwash/members/tests/test_models.py
@@ -1,17 +1,42 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
 from ..models import Member
 
 
 class MemberTest(TestCase):
+    def setUp(self):
+        self.member = Member()
+
     def test_add_new_member(self):
-        member = Member()
-        member.first_name = 'Kan'
-        member.last_name = 'Ouivirach'
-        member.email = 'zkan.cs@gmail.com'
+        self.member.first_name = 'Kan'
+        self.member.last_name = 'Ouivirach'
+        self.member.email = 'zkan.cs@gmail.com'
 
-        self.assertFalse(member.pk)
+        self.assertFalse(self.member.pk)
 
-        member.save()
+        self.member.save()
 
-        self.assertTrue(member.pk)
+        self.assertTrue(self.member.pk)
+
+    def test_add_new_member_without_first_name_should_fail(self):
+        self.member.first_name = None
+        self.member.last_name = 'Ouivirach'
+        self.member.email = 'zkan.cs@gmail.com'
+
+        self.assertRaises(IntegrityError, self.member.save)
+
+    def test_add_new_member_without_last_name_should_fail(self):
+        self.member.first_name = 'Kan'
+        self.member.last_name = None
+        self.member.email = 'zkan.cs@gmail.com'
+
+        self.assertRaises(IntegrityError, self.member.save)
+
+    def test_add_new_member_without_email_should_pass(self):
+        self.member.first_name = 'Kan'
+        self.member.last_name = 'Ouivirach'
+        self.member.email = None
+        self.member.save()
+
+        self.assertTrue(self.member.pk)
